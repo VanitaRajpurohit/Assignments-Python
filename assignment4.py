b4bef33a7c15e44a7f57990d651931e217b2aa64
VanitaRajpurohit <vanita.rajpurohit9819@gmail.com>
2021-08-01 20:45:22 +0530

    Update assignment4.py
    this file contains program to convert temperature from celsius to fahrenheit

--- assignment4.py
+++ assignment4.py
@@ -1,1 +1,12 @@
+i = 1
+while i:
+  tempC = input("Enter the temperature in celsius:")
+  try: 
+    tempC = float(tempC)
+    i = 0
+  except:
+    print("Please enter the temperature in numerical format")
 
+tempF = tempC * 9 / 5 + 32
+
+print("Output is: ", tempF)
